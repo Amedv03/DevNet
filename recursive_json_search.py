62f3507cc09a6da815a2852df84d477721366c4e
Amed <87840407+Amedv03@users.noreply.github.com>
2021-11-07 13:46:12 -0600

    Update recursive_json_search.py

--- recursive_json_search.py
+++ recursive_json_search.py
@@ -1,14 +1,14 @@
 from test_data import *
 def json_search(key,input_object):
-    ret_val= []
+    ret_val=[]
     if isinstance(input_object, dict): # Iterate dictionary
         for k, v in input_object.items(): # searching key in the dict
-            if k == clave:if k == key:
+            if k == key:
                 temp={k:v}
                 ret_val.append(temp)
             if isinstance(v, dict): # the value is another dict so repeat
-                json_search (key, v)
-            json_search(key,v)
+                json_search(key,v)
+            elif isinstance(v, list): # it's a list
                 for item in v:
                     if not isinstance(item, (str,int)): # if dict or list repeat
                         json_search(key,item)
@@ -16,5 +16,6 @@ def json_search(key,input_object):
         for val in input_object:
             if not isinstance(val, (str,int)):
                 json_search(key,val)
-    retorno ret_val
-print (json_search ("IssueSummary", data))
+    return ret_val
+print(json_search("issueSummary",data))
+
